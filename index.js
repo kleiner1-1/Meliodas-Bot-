55ab3f3a19766b3721b72f00e54df9d5e2d34f52
Bajobots1-1 <kleinergalindo4@gmail.com>
2025-08-05 00:24:25 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log('Iniciando 🚀🚀🚀') 
+import cfonts from 'cfonts';
+import chalk from 'chalk';
+
+cfonts.say('Rein-Bot', {
+  font: 'chrome',
+  align: 'center',
+  gradient: ['red', 'magenta'],
+  transition: false
+});
+
+cfonts.say('by: BajoBots', {
+  font: 'console',
+  align: 'center',
+  gradient: ['red', 'magenta'],
+  transition: false
+});
+
+//console.clear();
+
+import('./main.js');
